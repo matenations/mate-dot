df5e5e7c610f267fa2d3deee675199eecc6ad1d2
matenations <matenations@gmail.com>
2025-09-06 20:17:32 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,17 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-conic-180 from-gradient-3 from-0% to-gradient-4 to-100%" />
+      <div className="relative mx-auto mb-4 aspect-square w-20 sm:w-24 md:w-32 lg:w-40">
+        <Image
+          src="/logo.jpeg"   // place your logo in /public/logo.jpeg
+          alt="Site Logo"
+          fill
+          className="rounded-full object-cover"
+        />
+      </div>
       <p className="text-2xl text-center dark:text-white">
         <Link href="/">{name}</Link>
       </p>
